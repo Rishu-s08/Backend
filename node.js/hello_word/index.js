f8558daa55b30d3ac0536e8135e2f580526be609
rishi <rishi.2030s@gmail.com>
2025-07-12 15:42:09 +0530

    commit

---
+++ node.js/hello_word/index.js
@@ -0,0 +1,10 @@
+console.log('hello world')
+
+array = [1, 2, 3, 4, 5]
+console.log(array, 'array')
+
+setTimeout(() => {
+    console.log('after 2 seconds')
+}, 2000);
+
+console.log('last line of sync code')
